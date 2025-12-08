b4dd76eeb9ad79833caa87924fea6bf3f115f626
Fabio Salvemini <fsalvemi@cisco.com>
2025-12-08 22:13:51 +0100

    Add nac-catalystcenter-comprehensive-example

---
+++ nac-catalystcenter-comprehensive-example/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    catalystcenter = {
+      source  = "CiscoDevNet/catalystcenter"
+      version = "0.3.3"
+    }
+  }
+}
+
+provider "catalystcenter" {
+  username    = "username"
+  password    = "password"
+  url         = "https://dnac.url"
+  max_timeout = 600
+}
+
+module "catalyst_center" {
+  source  = "netascode/nac-catalystcenter/catalystcenter"
+  version = "0.2.0"
+
+  yaml_directories      = ["data/"]
+  templates_directories = ["data/templates/"]
+}
