d1e6e36e3df7dd8f53ee551430fbf9e9f44aa35f
Fabio Salvemini <fsalvemi@cisco.com>
2025-12-15 22:14:16 +0100

    Update nac-catalystcenter-comprehensive-example with latest upstream changes

--- nac-catalystcenter-comprehensive-example/main.tf
+++ nac-catalystcenter-comprehensive-example/main.tf
@@ -2,22 +2,24 @@ terraform {
   required_providers {
     catalystcenter = {
       source  = "CiscoDevNet/catalystcenter"
-      version = "0.3.3"
+      version = "0.4.5"
     }
   }
 }
 
 provider "catalystcenter" {
-  username    = "admin"
-  password    = "C1sco12345"
-  url         = "https://198.18.129.100"
+  username    = "username"
+  password    = "password"
+  url         = "https://dnac.url"
   max_timeout = 600
 }
 
 module "catalyst_center" {
   source  = "netascode/nac-catalystcenter/catalystcenter"
-  version = "0.2.0"
+  version = "0.3.0"
 
   yaml_directories      = ["data/"]
   templates_directories = ["data/templates/"]
+
+  use_bulk_api = true
 }
